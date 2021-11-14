b2d195ecb16c94ed001e5e61ba727fb11be56705
Ricardo Benitez <ricardo@ricardobenitez.xyz>
2021-11-14 12:15:24 +0100

    [Code] Removing the test_generated.h in root directory
    Signed-off-by: Ricardo Benitez <ricardo@ricardobenitez.xyz>

--- test_generated.h
+++
@@ -1,1 +0,0 @@
-/* Auto generated file */#ifndef TEST_GENERATED_H#define TEST_GENERATED_Hconst struct CMUnitTest tests[] ={cmocka_unit_test(create_linked_list_small_test),};#endif /* TEST_GENERATED_H
